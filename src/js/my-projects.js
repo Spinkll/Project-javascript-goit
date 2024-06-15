fef99f17432063388a747202f2b854a57806b04c
Kovaleva04 <156321094+Kovaleva04@users.noreply.github.com>
2024-06-15 15:11:25 +0300

    projects

--- src/js/my-projects.js
+++
@@ -1,6 +0,0 @@
-const images = document.querySelectorAll('.disable-click');
-
-images.forEach(img => {
-    img.classList.add('disabled-img');
-    img.addEventListener('click', event => event.preventDefault());
-});
